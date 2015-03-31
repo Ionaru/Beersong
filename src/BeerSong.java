427eeffb5f64f31764c68dee272f2dd646c23d07
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2015-03-31 12:09:24 +0200

    Fix layout

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,12 +1,11 @@
-
 public class BeerSong {
 
 	public static void main(String[] args) {
 		int beerNum = 100;
 		String word = "bottles";
-		
-		while (beerNum > 0){
-			if (beerNum == 1){
+
+		while (beerNum > 0) {
+			if (beerNum == 1) {
 				word = "bottle";
 			}
 			System.out.println();
@@ -15,18 +14,16 @@ public class BeerSong {
 			System.out.println("Take two down.");
 			System.out.println("Pass them around");
 			beerNum = beerNum - 2;
-			
-			if(beerNum > 0){
-				if(beerNum == 1){
+
+			if (beerNum > 0) {
+				if (beerNum == 1) {
 					word = "bottle";
 				}
-				System.out.println("You've got "+ beerNum + " " + word + " of beer on the wall");
+				System.out.println("You've got " + beerNum + " " + word
+						+ " of beer on the wall");
 			} else {
 				System.out.println("No more bottles of beer on the wall");
-				
 			}
 		}
-
 	}
-
 }
