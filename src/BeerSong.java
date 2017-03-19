cb7528d04cb1e3e08b0408129b14fff4240fd17c
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2017-03-19 03:32:22 +0100

    The song can now repeat multiple times!

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -2,54 +2,73 @@ public class BeerSong {
 
     public static void main(String[] args) {
         // Settings for the song
-        int drinksNumTotal = 99;
-        String containerSingle = "bottle";
-        String containerMultiple = "bottles";
-        String drink = "beer";
-        int numberOfDrinksToTake = 1;
+        final int songRuns = 1;
+
+        final int totalDrinksOnTheWall = 99;
+        final int numberOfDrinksToTake = 1;
+
+        final String containerSingle = "bottle";
+        final String containerMultiple = "bottles";
+        final String drinkName = "beer";
 
         // Song data
         String container = containerMultiple;
         String word;
+        int drinksLeft = totalDrinksOnTheWall;
+        int runsLeft = songRuns;
+        int drinksToTake;
 
-        while (drinksNumTotal > 0) {
+        while (runsLeft > 0) {
 
-            System.out.print('\n');
+            while (drinksLeft > 0) {
 
-            if (drinksNumTotal == 1) {
-                container = containerSingle;
-            }
+                System.out.print('\n');
 
-            System.out.print(drinksNumTotal + " " + container + " of "
-                    + drink + " on the wall, ");
-            System.out.print(drinksNumTotal + " " + container + " of "
-                    + drink + ".\n");
-
-            if (numberOfDrinksToTake > drinksNumTotal) {
-                numberOfDrinksToTake = drinksNumTotal;
-            }
-
-            System.out.print("Take " + numberOfDrinksToTake + " down");
-
-            if (numberOfDrinksToTake > 1) {
-                word = "them";
-            } else {
-                word = "it";
-            }
-
-            System.out.print(" and pass " + word + " around, ");
-            drinksNumTotal = drinksNumTotal - numberOfDrinksToTake;
-
-            if (drinksNumTotal > 0) {
-                if (drinksNumTotal == 1) {
+                if (drinksLeft == 1) {
                     container = containerSingle;
                 }
-                System.out.print("You've got " + drinksNumTotal + " "
-                        + container + " of " + drink + " on the wall.\n");
-            } else {
-                System.out.print("No more " + containerMultiple + " of "
-                        + drink + " on the wall!\n");
+
+                System.out.print(drinksLeft + " " + container + " of " + drinkName + " on the wall, ");
+                System.out.print(drinksLeft + " " + container + " of " + drinkName + ".\n");
+
+                drinksToTake = numberOfDrinksToTake;
+                if (numberOfDrinksToTake > drinksLeft) {
+                    drinksToTake = drinksLeft;
+                }
+
+                System.out.print("Take " + drinksToTake + " down");
+
+                if (drinksToTake > 1) {
+                    word = "them";
+                } else {
+                    word = "it";
+                }
+
+                System.out.print(" and pass " + word + " around, ");
+                drinksLeft = drinksLeft - drinksToTake;
+
+                if (drinksLeft > 0) {
+                    if (drinksLeft == 1) {
+                        container = containerSingle;
+                    }
+                    System.out.print(drinksLeft + " " + container + " of " + drinkName + " on the wall.\n");
+                } else {
+                    System.out.print("no more " + containerMultiple + " of " + drinkName + " on the wall.\n");
+                }
             }
+            runsLeft--;
+
+            if (runsLeft > 0) {
+                System.out.print('\n');
+                System.out.print("No more " + containerMultiple + " of " + drinkName + " on the wall, " +
+                        "no more + " + containerMultiple + " of " + drinkName + ".\n");
+
+                drinksLeft = totalDrinksOnTheWall;
+
+                System.out.print("Go to the store and buy some more, " +
+                        "" + drinksLeft + " " + container + " of " + drinkName + " on the wall.\n");
+            }
+
         }
     }
 }
