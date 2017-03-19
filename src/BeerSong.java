48dd0ef766e62095fd17aad6f97b7e64d3a71362
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2017-03-19 03:11:55 +0100

    Adjusted song formatting and code style

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,46 +1,55 @@
 public class BeerSong {
 
-	public static void main(String[] args) {
-		// Settings for the song
-		int drinksNumTotal = 100;
-		String containerSingle = "bottle";
-		String containerMultiple = "bottles";
-		String drink = "beer";
-		int numberOfDrinksToTake = 1;
+    public static void main(String[] args) {
+        // Settings for the song
+        int drinksNumTotal = 99;
+        String containerSingle = "bottle";
+        String containerMultiple = "bottles";
+        String drink = "beer";
+        int numberOfDrinksToTake = 1;
 
-		// Song data
-		String container = containerMultiple;
-		String word;
+        // Song data
+        String container = containerMultiple;
+        String word;
 
-		while (drinksNumTotal > 0) {
-			if (drinksNumTotal == 1) {
-				container = containerSingle;
-			}
-			System.out.println();
-			System.out.println(drinksNumTotal + " " + container + " of "
-					+ drink + " on the wall");
-			System.out.println(drinksNumTotal + " " + container + " of "
-					+ drink + ".");
-			System.out.println("Take " + numberOfDrinksToTake + " down.");
+        while (drinksNumTotal > 0) {
 
-			if (numberOfDrinksToTake > 1) {
-				word = "them";
-			} else {
-				word = "it";
-			}
-			System.out.println("Pass " + word + " around");
-			drinksNumTotal = drinksNumTotal - numberOfDrinksToTake;
+            System.out.print('\n');
 
-			if (drinksNumTotal > 0) {
-				if (drinksNumTotal == 1) {
-					container = containerSingle;
-				}
-				System.out.println("You've got " + drinksNumTotal + " "
-						+ container + " of " + drink + " on the wall");
-			} else {
-				System.out.println("No more " + containerMultiple + " of "
-						+ drink + " on the wall");
-			}
-		}
-	}
+            if (drinksNumTotal == 1) {
+                container = containerSingle;
+            }
+
+            System.out.print(drinksNumTotal + " " + container + " of "
+                    + drink + " on the wall, ");
+            System.out.print(drinksNumTotal + " " + container + " of "
+                    + drink + ".\n");
+
+            if (numberOfDrinksToTake > drinksNumTotal) {
+                numberOfDrinksToTake = drinksNumTotal;
+            }
+
+            System.out.print("Take " + numberOfDrinksToTake + " down");
+
+            if (numberOfDrinksToTake > 1) {
+                word = "them";
+            } else {
+                word = "it";
+            }
+
+            System.out.print(" and pass " + word + " around, ");
+            drinksNumTotal = drinksNumTotal - numberOfDrinksToTake;
+
+            if (drinksNumTotal > 0) {
+                if (drinksNumTotal == 1) {
+                    container = containerSingle;
+                }
+                System.out.print("You've got " + drinksNumTotal + " "
+                        + container + " of " + drink + " on the wall.\n");
+            } else {
+                System.out.print("No more " + containerMultiple + " of "
+                        + drink + " on the wall!\n");
+            }
+        }
+    }
 }
